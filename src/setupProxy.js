b7e98ef1c056d3fb1b9e63462aaccea90e69038e
Dov Dov <tablefloorchair23@gmail.com>
2021-01-12 18:27:16 +0200

    added s

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         '/token',
         createProxyMiddleware({
-            target: 'http://3.16.152.174:8000',
+            target: 'https://3.16.152.174:8000',
             changeOrigin: true,
             pathRewrite: {
                 '^/token': ""
