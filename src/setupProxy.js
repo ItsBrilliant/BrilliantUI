c8ff657b9f85cc42c95388b51c6fae7dd5f7d76e
Dov Dov <tablefloorchair23@gmail.com>
2021-01-12 20:45:36 +0200

    added miclog

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,10 +4,20 @@ module.exports = function (app) {
     app.use(
         '/token',
         createProxyMiddleware({
-            target: 'https://3.16.152.174:8000',
+            target: 'http://3.16.152.174:8001',
             changeOrigin: true,
             pathRewrite: {
                 '^/token': ""
+            },
+        })
+    );
+    app.use(
+        '/miclog',
+        createProxyMiddleware({
+            target: 'https://3.16.152.174:8000',
+            changeOrigin: true,
+            pathRewrite: {
+                '^/miclog': ""
             },
         })
     );
