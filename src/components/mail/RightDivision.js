9dbef87e096172542dcd4211162f3d7368b63423
Dov Bridger <dovbridger@itsbrilliant.com>
2021-02-09 16:28:17 +0200

    added default message to task dispplayer when there are no tasks

--- src/components/mail/RightDivision.js
+++ src/components/mail/RightDivision.js
@@ -26,9 +26,6 @@ export function RightDivision(props) {
 function Tasks(props) {
     const dispatch = useDispatch();
     const task_updater = (task) => dispatch(Update(task));
-
-    //Old function used to highlight
-    const on_task_click = props.on_task_click;
     const [task_id_for_info, set_task_id] = useState(null);
     const finished_tasks = props.tasks.filter(task => task.isDone);
     const active_tasks = props.tasks.filter(task => !task.isDone);
@@ -36,48 +33,46 @@ function Tasks(props) {
         Task.update_task(task_updater, task, 'set_status', ["Done"]);
     }
 
-    if (finished_tasks.length === 0 && active_tasks.length === 0) {
-        return null;
-    } else {
-        return (
-            <div className='Container'>
-                <TaskInfoWrapper thread={props.thread}
-                    task_id={task_id_for_info}
-                    close={() => set_task_id(null)} />
-                <TasksDisplayer tasks={active_tasks} areDone={false} on_status_click={my_update_finished_task} on_click={set_task_id} />
-                <TasksDisplayer tasks={finished_tasks} areDone={true} on_click={set_task_id} />
-            </div>
-        )
-    }
-
-
+    return (
+        <div className='Container'>
+            <TaskInfoWrapper thread={props.thread}
+                task_id={task_id_for_info}
+                close={() => set_task_id(null)} />
+            <TasksDisplayer tasks={active_tasks} areDone={false} on_status_click={my_update_finished_task} on_click={set_task_id} />
+            <TasksDisplayer tasks={finished_tasks} areDone={true} on_click={set_task_id} />
+        </div>
+    )
 }
+
 
 function TasksDisplayer(props) {
     const user = useSelector(state => state.user);
-    if (props.tasks.length === 0) {
+    const empty = props.tasks.length === 0
+    if (empty && props.areDone) {
         return null;
     }
     const title = props.areDone ? "Done" : "Priority Tasks"
     const done_style = props.areDone ? " done" : " not_done"
-    const tasks_elements = props.tasks.map(task => {
-        const owner_name = user === task.owner ? "You" : task.owner.get_first_name();
-        return (
-            <li className={get_priority_style(task.priority)}>
-                <span className={"task_status_button"}
-                    onClick={() => { if (props.on_status_click) { props.on_status_click(task); } }}>
-                    <span className="inner">v</span>
-                </span>
-                <span className={"task_owner" + done_style}>{owner_name}:</span>
-                <span onClick={() => props.on_click(task.get_id())} className="task_text">{task.text}</span>
-            </li>
-        );
-    })
-    //show deadline ->      + " (due: " + format_date(task.deadline).date + ")"
+    const body = empty ? <h5>No tasks yet</h5> :
+        <ul>{
+            props.tasks.map(task => {
+                const owner_name = user === task.owner ? "You" : task.owner.get_first_name();
+                return (
+                    <li key={task.id} className={get_priority_style(task.priority)}>
+                        <span className={"task_status_button"}
+                            onClick={() => { if (props.on_status_click) { props.on_status_click(task); } }}>
+                            <span className="inner">v</span>
+                        </span>
+                        <span className={"task_owner" + done_style}>{owner_name}:</span>
+                        <span onClick={() => props.on_click(task.get_id())} className="task_text">{task.text}</span>
+                    </li>
+                );
+            })
+        }</ul>
     return (
         <div className={'TasksDisplayer' + done_style}>
             <h4>{title}</h4>
-            <ul>{tasks_elements}</ul>
+            {body}
         </div>
     );
 }
@@ -94,7 +89,7 @@ function Participants(contacts) {
 
     return (
         <div className="Container">
-            <h5>People in the Conversation</h5>
+            <h5>People in Conversation</h5>
             <div className='ParticipantsIcons'>
                 {icons}
             </div>
